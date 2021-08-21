cca42a40de8416e34094d2bb916c7c77dc0e4d73
CmdData <61318415+danielbuiles@users.noreply.github.com>
2021-08-21 20:47:38 +0000

    3r problem resolve

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function Problem1(x1,y1,x2=0,y2=0) {
+//funciones con los 5 problemas
+function distanciaPlanetaria(x1,y1,x2=0,y2=0) {
     return  Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
 }
 
-function problem2(clave) {
-    return clave.split(" ").splice(1);
+function descifradorPiloto(clave){
+  return  clave.split(":").slice(1);
 }
 
-console.log("Problema N1= ",Problem1(50,70));
-console.log("Problema N2= ",...problem2("SHJWER3D3: Juana Camellin"));
+const temperaturaEndor = (max,min) =>{
+  return (max+min)/2;
+}
+
+//escribe los resultados  por consola
+console.log("Problema N1= ",distanciaPlanetaria(50,70));
+console.log("Problema N2= ",...descifradorPiloto("ARQ20055: Noddin Chavrin"));
+console.log(temperaturaEndor(27,10));
