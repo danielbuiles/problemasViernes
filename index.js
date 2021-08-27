fea81526a049a726b7a3aab9b354661a76980b78
CmdData <61318415+danielbuiles@users.noreply.github.com>
2021-08-27 12:25:17 +0000

    remasterisacion del codigo en clase

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function distanciaPlanetaria(x1,y1,x2=0,y2=0) {
     return  Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
 }
 
-function descifradorPiloto(clave){
-  return  clave.split(":").slice(1);
+function descifradorPiloto(codNave){
+  return  codNave.split(":").slice(1);
 }
 
-const temperaturaEndor = (max,min) =>{
-  return (max+min)/2;
+const calcularTemperaturaPromedio = Temperatura =>{
+  return (Math.min(...Temperatura)+Math.max(...Temperatura))/2;
 }
 
-function monitoreoSables(arr){
+function monitoreoSables(sables){
   let energiaOscura=0;
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]<0){
+  for(let i=0;i<sables.length;i++){
+    if(sables[i]<0){
       energiaOscura++;
     }
   }
@@ -22,7 +22,7 @@ function monitoreoSables(arr){
 }
 
 //escribe los resultados  por consola haga sus propios testeos
-console.log("Problema N1= ",distanciaPlanetaria(50,70));
-console.log("Problema N2= ",...descifradorPiloto("ARQ20055: Noddin Chavrin"));
-console.log("Problema N3= ",temperaturaEndor(27,10));
-console.log("Problema N4= ",monitoreoSables([2,4-8,5,-6]));
+console.log(`Distancia entre los planetas= ${distanciaPlanetaria(50,70)}`);
+console.log(`Nombre del piloto de la Nave=${descifradorPiloto("ARQ20055: Noddin Chavrin")}`);
+console.log(`Temperatura promediode endor= ${calcularTemperaturaPromedio([10,50,60,20,5])}C`);
+console.log(`cantidad de sables con energia negativa= ${monitoreoSables([2,4-8,5,-6])}`);
