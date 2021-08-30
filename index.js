ae17c4092005d4ad1dbf1385e9b42310dfe8ae66
CmdData <61318415+danielbuiles@users.noreply.github.com>
2021-08-30 11:43:09 +0000

    pequeños cambios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const calcularTemperaturaPromedio = Temperatura =>{
   return (Math.min(...Temperatura)+Math.max(...Temperatura))/2;
 }
 
-function monitoreoSables(sables){
+function numeroSablesOscuros(sables){
   let energiaOscura=0;
   for(let i=0;i<sables.length;i++){
     if(sables[i]<0){
@@ -21,8 +21,11 @@ function monitoreoSables(sables){
   return energiaOscura;
 }
 
+const saludoFlecha = saludo => `hello madafa.. ${saludo}`
+
 //escribe los resultados  por consola haga sus propios testeos
 console.log(`Distancia entre los planetas= ${distanciaPlanetaria(50,70)}`);
 console.log(`Nombre del piloto de la Nave=${descifradorPiloto("ARQ20055: Noddin Chavrin")}`);
 console.log(`Temperatura promediode endor= ${calcularTemperaturaPromedio([10,50,60,20,5])}C`);
-console.log(`cantidad de sables con energia negativa= ${monitoreoSables([2,4-8,5,-6])}`);
+console.log(`cantidad de sables con energia negativa= ${numeroSablesOscuros([2,4-8,5,-6])}`);
+console.log(saludoFlecha("Kamill"))
